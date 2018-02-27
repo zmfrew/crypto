fd5ee7013be0ebdcc3c996cb84133c72669d93a5
zfrew <z_frew@yahoo.com>
2018-02-26 18:32:50 -0600

    create vigenere encrypt function, refactor repeated code into helpers.py, and import helper functions

---
+++ vigenere.py
@@ -0,0 +1,37 @@
+def alphabet_position(letter):
+    return ord(letter.lower()) - 97
+
+def rotate_character(char, rot):
+    char_index = alphabet_position(char)
+    if char.isalpha():
+        new_char = chr((char_index + rot) % 26 + 97)
+        if char.isupper():
+            new_char = new_char.upper()
+    else:
+        return char
+    return new_char
+
+def encrypt(text, e_key):
+    text_list = list(text)
+    e_key_list = list(e_key)
+    new_text_list = []
+    idx = 0
+    for index, char in enumerate(text_list):
+        if idx == len(e_key):
+            idx = 0
+        char_index = alphabet_position(e_key_list[idx])
+        new_text_list.append(rotate_character(char, char_index))
+        if text_list[index] == " ":
+            idx = idx
+        else:
+            idx += 1
+    new_text = "".join(new_text_list)
+    return new_text
+
+def main():
+    user_text = input("Type a message:")
+    user_rot = input("Encryption key:")
+    print(encrypt(user_text, user_rot))
+
+if __name__ == "__main__":
+    main()
